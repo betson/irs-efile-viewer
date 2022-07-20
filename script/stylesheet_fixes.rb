36334dfe4ae7f529f2ac542108c15185b53b4206
betson <betson@users.noreply.github.com>
2022-07-19 20:56:11 -0700

    log all stylesheet fixes applied to scripts, styles, and stylesheets
    This list should be manually updated each time a new fix is applied on top
    of the default IRS stylesheet files. It can serve as a reference when adding
    new tax years to ensure that we're A) not overwriting changes that were
    previously necessary; and B) identifying standard changes that must be
    applied to new tax years.

---
+++ script/stylesheet_fixes.rb
@@ -0,0 +1,143 @@
+STYLESHEET_FIXES = {
+    "2008": {
+        stylesheets: {
+            CommonPathRef: ["04a28d1"],
+            IRS1065B: ["04a28d1"],
+            PrintDocList: ["04a28d1"],
+            PrintReturnSections: ["04a28d1"]
+        },
+        scripts: {
+            FormDisplay: ["04a28d1"]
+        }
+    },
+    "2009": {
+        stylesheets: {
+            CommonPathRef: ["04a28d1"],
+            IRS1065B: ["04a28d1"],
+            PrintDocList: ["04a28d1"],
+            PrintReturnSections: ["04a28d1"]
+        },
+        scripts: {
+            FormDisplay: ["04a28d1"]
+        }
+    },
+    "2010": {
+        stylesheets: {
+            CommonPathRef: ["04a28d1"],
+            PrintDocList: ["04a28d1"],
+            PrintReturnSections: ["04a28d1"]
+        },
+        scripts: {
+            FormDisplay: ["04a28d1"]
+        }
+    },
+    "2011": {
+        stylesheets: {
+            CommonPathRef: ["04a28d1"],
+            PrintDocList: ["04a28d1"],
+            PrintReturnSections: ["04a28d1"]
+        },
+        scripts: {
+            FormDisplay: ["04a28d1"]
+        },
+        styles: {
+            style: ["04a28d1"]
+        }
+    },
+    "2012": {
+        stylesheets: {
+            CommonPathRef: ["04a28d1"],
+            IRS1040SSPRStyle: ["04a28d1"],
+            PrintDocList: ["04a28d1"],
+            PrintReturnSections: ["04a28d1"]
+        },
+        scripts: {
+            FormDisplay: ["04a28d1"]
+        },
+        styles: {
+            style: ["04a28d1"]
+        }
+    },
+    "2013": {
+        stylesheets: {
+            CommonPathRef: ["04a28d1"],
+            IRS940_940PRStyle: ["04a28d1"],
+            IRS941ScheduleBStyle: ["04a28d1"],
+            IRS990: ["b57ff68"],
+            PrintDocList: ["04a28d1"],
+            PrintReturnSections: ["04a28d1"]
+        },
+        scripts: {
+            FormDisplay: ["04a28d1"]
+        },
+        styles: {
+            header: ["e71ed26"],
+            style: ["04a28d1"]
+        }
+    },
+    "2014": {
+        stylesheets: {
+            CommonPathRef: ["04a28d1"],
+            IRS940_940PRStyle: ["04a28d1"],
+            IRS941ScheduleBStyle: ["04a28d1"],
+            IRS990: ["b57ff68"],
+            IRS990PF: ["6cbd8cb"],
+            PrintDocList: ["04a28d1"],
+            PrintReturnSections: ["04a28d1"]
+        },
+        scripts: {
+            FormDisplay: ["04a28d1"]
+        },
+        styles: {
+            header: ["e71ed26"],
+            style: ["04a28d1"]
+        }
+    },
+    "2015": {
+        stylesheets: {
+            CommonPathRef: ["04a28d1"],
+            IRS940_940PRStyle: ["04a28d1"],
+            IRS941ScheduleBStyle: ["04a28d1"],
+            IRS990: ["b57ff68"],
+            IRS990PF: ["6cbd8cb"],
+            PrintDocList: ["04a28d1"],
+            PrintReturnSections: ["04a28d1"]
+        },
+        scripts: {
+            FormDisplay: ["04a28d1"]
+        },
+        styles: {
+            header: ["e71ed26"],
+            style: ["04a28d1"]
+        }
+    },
+    "2016": {
+        stylesheets: {
+            IRS940_940PRStyle: ["04a28d1"],
+            IRS941ScheduleBStyle: ["04a28d1"],
+            PrintDocList: ["04a28d1"],
+            PrintReturnSections: ["04a28d1"]
+        },
+        scripts: {
+            FormDisplay: ["04a28d1"]
+        },
+        styles: {
+            header: ["04a28d1"],
+            style: ["04a28d1"]
+        }
+    },
+    "2017": {
+        stylesheets: {
+            IRS941ScheduleBStyle: ["04a28d1"],
+            PrintDocList: ["04a28d1"],
+            PrintReturnSections: ["04a28d1"]
+        },
+        scripts: {
+            FormDisplay: ["04a28d1"]
+        },
+        styles: {
+            header: ["04a28d1"],
+            style: ["04a28d1"]
+        }
+    }
+}
